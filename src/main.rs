53d4ce12e7cec42905656fc318fdc97c98fb8478
Jibbril Berbres <jibbril.berbres@viego.se>
2023-07-05 01:14:22 +0200

    Clear unused import

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-extern crate dotenv;
-
 use dotenv::dotenv;
-use std::env;
-
 
 fn main() {
     dotenv().ok();
-
-    for (key,value) in env::vars() {
-        if key == "ALPHA_VANTAGE_KEY" {
-            println!("{}, {}",key,value);
-        }
-    }
 }
