67c3bf45ea624bdf6a113bfc460c02e61b604e12
Jibbril Berbres <jibbril.berbres@viego.se>
2023-10-21 21:00:39 +0200

    RB-2 Refactor connect_ws

--- src/data_sources/bybit/ws/mod.rs
+++ src/data_sources/bybit/ws/mod.rs
@@ -11,19 +11,36 @@ use crate::{
 use actix::Addr;
 use anyhow::Result;
 use futures_util::{SinkExt, StreamExt};
-use tokio_tungstenite::connect_async;
-use tungstenite::Message;
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, WebSocketStream, MaybeTlsStream};
+use tungstenite::{Message, Error};
+use self::incoming_message::KlineResponse;
 
 pub async fn connect_ws(client: &Addr<WebsocketClient>) -> Result<()> {
     let url = "wss://stream-testnet.bybit.com/v5/public/spot";
     let (mut ws_stream, _) = connect_async(url).await?;
 
-    // Send ping
+    send_ping(&mut ws_stream).await?;
+    subscribe_to_kline(&mut ws_stream).await?;
+
+    while let Some(msg) = ws_stream.next().await {
+        handle_message(msg, client).await?;
+    }
+
+    ws_stream.close(None).await?;
+    Ok(())
+}
+
+
+async fn send_ping(ws_stream: &mut WebSocketStream<MaybeTlsStream<TcpStream>>) -> Result<()> {
     let ping = OutgoingMessage::ping();
     let message = Message::Text(ping.to_json());
     ws_stream.send(message).await?;
 
-    // Subscribe to kline
+    Ok(())
+}
+
+async fn subscribe_to_kline(ws_stream: &mut WebSocketStream<MaybeTlsStream<TcpStream>>) -> Result<()> {
     let args = vec![OutgoingMessageArg {
         stream: "kline".to_string(),
         interval: "1".to_string(),
@@ -34,44 +51,40 @@ pub async fn connect_ws(client: &Addr<WebsocketClient>) -> Result<()> {
 
     ws_stream.send(Message::Text(json)).await?;
 
-    let i = 0;
-    while let Some(msg) = ws_stream.next().await {
-        let msg = msg?;
+    Ok(())
+}
 
-        if let Message::Text(txt) = msg {
-            // let v: serde_json::Value = serde_json::from_str(txt.as_str())?;
-            let parsed: IncomingMessage = serde_json::from_str(txt.as_str())?;
+async fn handle_message(msg: Result<Message,Error>, client: &Addr<WebsocketClient>) -> Result<()> {
+    let msg = msg?;
 
-            match parsed {
-                IncomingMessage::Pong(pong) => {
-                    println!("Pong: {:#?}", pong);
-                }
-                IncomingMessage::Subscribe(sub) => {
-                    println!("Subscribe: {:#?}", sub);
-                }
-                IncomingMessage::Kline(kline_response) => {
-                    let kline = kline_response.get_kline()?;
+    if let Message::Text(txt) = msg {
+        // let v: serde_json::Value = serde_json::from_str(txt.as_str())?;
+        let parsed: IncomingMessage = serde_json::from_str(txt.as_str())?;
 
-                    if !kline.confirm {
-                        // TODO: Change to taking the next candle instead of the confirmed one. Solves issue with timestamps being wrong.
-                        continue;
-                    }
-
-                    let candle = kline.to_candle()?;
-                    let payload = WebsocketPayload {
-                        ok: true,
-                        message: Some(i.to_string()),
-                        candle: Some(candle),
-                    };
-
-                    client.do_send(payload);
-                }
-            }
+        match parsed {
+            IncomingMessage::Pong(pong) => println!("Pong: {:#?}", pong),
+            IncomingMessage::Subscribe(sub) => println!("Subscribe: {:#?}", sub),
+            IncomingMessage::Kline(kline_response) => handle_kline(kline_response, client).await?,
         }
     }
 
-    println!("Done");
+    Ok(())
+}
 
-    ws_stream.close(None).await?;
+async fn handle_kline(kline_response: KlineResponse, client: &Addr<WebsocketClient>) -> Result<()> {
+    let kline = kline_response.get_kline()?;
+
+    if kline.confirm {
+        // TODO: Change to taking the next candle instead of the confirmed one. Solves issue with timestamps being wrong.
+        let candle = kline.to_candle()?;
+        let payload = WebsocketPayload {
+            ok: true,
+            message: None,
+            candle: Some(candle),
+        };
+
+        client.do_send(payload);
+    }
+
     Ok(())
 }
