f7e5f0e2e3393fcd3439a780516f8ee9a640917b
Jibbril Berbres <jibbril.berbres@viego.se>
2024-01-14 22:27:51 +0100

    Create IsResolutionStrategy trait

---
+++ src/resolution_strategies/is_resolution_strategy.rs
@@ -0,0 +1,15 @@
+use crate::models::{strategy_orientation::StrategyOrientation, candle::Candle};
+
+pub trait IsResolutionStrategy {
+    /// Number of candles needed to check whether stop-loss has been reached
+    fn n_candles_stop_loss(&self) -> usize;
+
+    /// Number of candles needed to check whether take-profit has been reached
+    fn n_candles_take_profit(&self) -> usize;
+
+    /// Check whether stop-loss has been reached for the last candle
+    fn stop_loss_reached(&self, orientation: &StrategyOrientation, candles: &[Candle]) -> Result<bool>;
+
+    /// Check whether take-profit has been reached for the last candle
+    fn take_profit_reached(&self, orientation: &StrategyOrientation, candles: &[Candle]) -> Result<bool>;
+}
